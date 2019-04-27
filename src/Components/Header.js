b27b117853330b2c39abad499688a5853d732abc
CCGS <ccgs2014@gmail.com>
2019-04-27 13:56:20 -0400

    adding components

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+
+<div className="logo">
+ <a data-toggle="collapse" data-target=".navbar-collapse.show" class="navbar-brand" href="index-kenburns.html">
+    <img id="logo-light" class="logo-light" src="http://via.placeholder.com/159x28" alt="logo-light" />
+    <img id="logo-dark" class="logo-dark" src="http://via.placeholder.com/159x28" alt="logo-dark" />
+ </a>
+</div>
+);
+
+export default Header;
---
+++ src/Components/SearchInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const SearchInput = () => (
+
+<div className="site-search hidden-xs" className="search-container">
+	<input id="search-input" type="text" placeholder="type your keyword and hit enter ..."/>
+    <span className="close">×</span>
+</div>
+   
+
+
+
+
+
+);
+
+
+export default SearchInput;
