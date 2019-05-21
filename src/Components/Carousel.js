50f49769beeeb124790cbbd5ff2a0f749f8f6e38
CCGS <ccgs2014@gmail.com>
2019-05-21 14:23:46 -0400

    working on Css

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -74,6 +74,18 @@
               onExiting={this.onExiting}
               onExited={this.onExited}
             >
+<div className="testimonials">
+<div id="quote-carousel" className="carousel slide" data-ride="carousel">
+<div className="item active">
+    <blockquote>
+        <img className="img-circle img-responsive" src="http://via.placeholder.com/112x112" alt="client" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu nt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptat</p>
+        <h5>Miss Elina Pool</h5>
+        <h6>Developer - Adobe</h6>
+    </blockquote>
+</div>
+</div>
+</div>
               <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
             </CarouselItem>
           );
